68273bf6018e76787cdb698e01058eb67f04709f
TimendusBellator <77513741+TimendusBellator@users.noreply.github.com>
2021-07-29 16:08:57 +0200

    changement du titre

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   div
     nav.navbar.navbar-expand-lg.navbar-dark.bg-dark
       .container-fluid
-        nuxt-link(to="/" class="navbar-brand") NUXT-MONGO-CRUD
+        nuxt-link(to="/" class="navbar-brand") NUXT-APP-PERSO
         button.navbar-toggler(type='button' data-bs-toggle='collapse' data-bs-target='#navbarSupportedContent' aria-controls='navbarSupportedContent' aria-expanded='false' aria-label='Toggle navigation')
           span.navbar-toggler-icon
         #navbarSupportedContent.collapse.navbar-collapse
